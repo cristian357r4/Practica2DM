e146aa16d5716e9c307a033fc863eeb813c48308
xazak <cristian357r4@gmail.com>
2021-03-05 12:00:33 -0600

    Ejemplo de la pagina de packet https://hub.packtpub.com/regression-models-weka/

---
+++ src/main/java/Main.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import weka.core.Instance;
+import weka.core.Instances;
+import weka.classifiers.functions.LinearRegression;
+
+public class Main {
+    public static void main(String args[]) throws Exception {
+//load data
+        Instances data = new Instances(new BufferedReader(new
+                FileReader("dataset/linear.arff")));
+        data.setClassIndex(data.numAttributes() - 1);
+//build model
+        LinearRegression model = new LinearRegression();
+        model.buildClassifier(data); //the last instance with missing class is not used
+        System.out.println(model);
+
+//classify the last instance
+        Instance myHouse = data.lastInstance();
+        double price = model.classifyInstance(myHouse);
+        System.out.println("My house (" + myHouse + "): " + price);
+    }
+}
