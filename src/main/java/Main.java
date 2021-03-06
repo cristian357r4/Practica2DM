6a03f23aa4f4a4efa1448e34d9b24b2b29b45803
xazak <cristian357r4@gmail.com>
2021-03-06 09:01:55 -0600

    Create custom instance

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,13 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
+
+import weka.core.DenseInstance;
 import weka.core.Instance;
 import weka.core.Instances;
 import weka.classifiers.functions.LinearRegression;
 
 public class Main {
-    public static void main(String args[]) throws Exception {
+    public static void main(String[] args) throws Exception {
 //load data
         Instances data = new Instances(new BufferedReader(new
                 FileReader("dataset/linear.arff")));
@@ -17,7 +19,15 @@ public class Main {
 
 //classify the last instance
         Instance myHouse = data.lastInstance();
+        Instance test = new DenseInstance(6);
+        //estos valores se llenan con los jtexfield
+        test.setValue(0, 800);
+        test.setValue(1, 3000);
+        test.setValue(2, 6);
+        test.setValue(3, 1);
+        test.setValue(4, 1);
         double price = model.classifyInstance(myHouse);
+        double price1 = model.classifyInstance(test);
         System.out.println("My house (" + myHouse + "): " + price);
     }
 }
